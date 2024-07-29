923ad291bef4f4a1f158b70fd166f2e92de52dc3
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-07-29 23:33:36 +0530

    done

---
+++ React_JS/script1.js
@@ -0,0 +1,16 @@
+// In javaScript arryays are dynamic it can store multiple types in a single array.
+// eg:- arr[1,2,3,"aman"]
+
+// var arr= [1,2,3,4];
+// var arr2= arr;
+// console.log(arr2.pop());
+// console.log(arr,arr2);
+
+// var state=[1,2,3,4];
+// copy=[...state];
+// copy.pop();
+
+// var state={name:"harsh",age:24};
+// var copy={...state};
+// copy.name="riya";
+// state=copy;
